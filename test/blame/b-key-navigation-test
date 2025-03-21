ilyagr/tig

commit ca0809dbd20a31f6fa9b84c7eed5ecaa1368cd9d

Fix cursor position after "Move to parent" in blame view
Fixes #973

src/blame.c
@@ -425,7 +425,7 @@ blame_go_forward(struct view *view, struct blame *blame, bool parent)
 	string_ncopy(view->env->file, filename, strlen(filename));
 	if (parent)
 		setup_blame_parent_line(view, blame);
-	view->env->goto_lineno = blame->lineno;
+	view->env->goto_lineno = view->pos.lineno;
 	reload_view(view);
 }
 

commit 7312d071bd8e344c90c273e9f8ff8bf3f17ac1ae

Use correct line from parent on recursive blame on blob
Running

	tig blame +28 tig-2.5.12 -- src/diff.c

and pressing "b" should keep the line containing "const char *diff_argv[]
= }" selected. It actually moves the selection somewhere else.

This regressed in ca0809db (Fix cursor position after "Move to parent"
in blame view, 2019-11-29).

Commit ca0809db was only about "," (":back" or ":parent").  When we press
"b" (":view-blame"), we want to use the old behavior (originally added in
ba7c7d30 (Use file and line number information when loading blame for commit,
2009-02-07)). Do that.

(Note that the behavior fixed by ca0809db regressed in 22807341 (Enable
textconv in the blame view and fix blame -L (#1190), 2022-06-03).  I have
not yet figured out what's going on, but this patch shouldn't make that worse.)

Fixes #1369

src/blame.c
@@ -373,9 +373,12 @@ blame_go_forward(struct view *view, struct blame *blame, bool parent)
 
 	string_ncopy(view->env->ref, id, sizeof(commit->id));
 	string_ncopy(view->env->file, filename, strlen(filename));
-	if (parent)
+	if (parent) {
 		setup_blame_parent_line(view, blame);
-	view->env->goto_lineno = view->pos.lineno;
+		view->env->goto_lineno = view->pos.lineno;
+	} else {
+		view->env->goto_lineno = blame->lineno - 1;
+	}
 	reload_view(view);
 }
 

test/blame/b-key-navigation-test
@@ -1,0 +1,58 @@
+#!/bin/sh
+
+. libtest.sh
+. libgit.sh
+
+export COLUMNS=200
+
+# This test verifies the fix for issue #1369
+# The test checks that pressing 'b' in blame view correctly navigates
+# to the corresponding line in the target commit.
+
+in_work_dir create_repo_from_tgz "$base_dir/files/scala-js-benchmarks.tgz"
+
+tigrc <<EOF
+set line-graphics = ascii
+EOF
+
+steps '
+	# Press b to go to blame for that line/commit
+	:view-blame
+	:save-display recursive-blame.screen
+'
+
+test_tig blame +27 project/Build.scala
+
+# This shows that after pressing 'b' we should be positioned the line starting
+# with "lazy val parent".
+assert_equals 'recursive-blame.screen' <<EOF
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  18|       scalaVersion := scalajsScalaVersion,
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  19|       scalacOptions ++= Seq(
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  20|           "-deprecation",
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  21|           "-unchecked",
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  22|           "-feature",
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  23|           "-encoding", "utf8"
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  24|       )
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  25|   )
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  26|
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  27|   lazy val benchmarkSettings = defaultSettings ++ Seq(
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  28|       unmanagedSources in (Compile, packageJS) +=
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  29|           baseDirectory.value / "exports.js"
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  30|   )
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  31|
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  32|   lazy val parent: Project = Project(
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  33|       id = "parent",
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  34|       base = file("."),
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  35|       settings = projectSettings ++ Seq(
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  36|           name := "Scala.js Benchmarks",
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  37|           publishArtifact in Compile := false,
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  38|
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  39|           clean := clean.dependsOn(
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  40|              clean in common,
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  41|              clean in tracer
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  42|           ).value
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  43|       )
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  44|   ).aggregate(
+90286e0 Jonas Fonseca 2013-10-14 14:56 -0400  45|       common,
+[blame] 90286e0752016a6bca30dfa7ca236d1f99345eb8 changed project/Build.scala - line 32 of 66 68%
+EOF